6226f9999dbbdcc4c457548eed4f70ffbe59ba64
Dejan Tomic <hi@iamdejan.com>
2026-07-24 16:58:02 +0200

    back: the assets item ships the front to the browser

---
+++ back/items/onetype/assets/transforms.js
@@ -0,0 +1,8 @@
+onetype.assets.ItemAdd({
+    id: 'transforms',
+    description: 'The transforms engine, behavior and effects on plain markup.',
+    addon: 'transforms',
+    js: '../../../front',
+    css: '../../../front',
+    url: import.meta.url
+});
